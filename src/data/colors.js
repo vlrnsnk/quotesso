a43c94cdc486c7e21d52869fce071865622c371c
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-08-13 07:54:40 -0400

    Add all the colors from tailwind css

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,5 +1,30 @@
 const colors = [
   {
+    name: 'slate',
+    lightColor: '#f8fafc',
+    darkColor: '#0f172a',
+  },
+  {
+    name: 'gray',
+    lightColor: '#f9fafb',
+    darkColor: '#030712',
+  },
+  {
+    name: 'zinc',
+    lightColor: '#fafafa',
+    darkColor: '#18181b',
+  },
+  {
+    name: 'neutral',
+    lightColor: '#fafafa',
+    darkColor: '#171717',
+  },
+  {
+    name: 'stone',
+    lightColor: '#fafaf9',
+    darkColor: '#1c1917',
+  },
+  {
     name: 'red',
     lightColor: '#fef2f2',
     darkColor: '#7f1d1d',
@@ -8,7 +33,82 @@ const colors = [
     name: 'orange',
     lightColor: '#fff7ed',
     darkColor: '#7c2d12',
-  }
+  },
+  {
+    name: 'amber',
+    lightColor: '#fffbeb',
+    darkColor: '#78350f',
+  },
+  {
+    name: 'yellow',
+    lightColor: '#fefce8',
+    darkColor: '#713f12',
+  },
+  {
+    name: 'lime',
+    lightColor: '#f7fee7',
+    darkColor: '#365314',
+  },
+  {
+    name: 'green',
+    lightColor: '#f0fdf4',
+    darkColor: '#14532d',
+  },
+  {
+    name: 'emerald',
+    lightColor: '#ecfdf5',
+    darkColor: '#064e3b',
+  },
+  {
+    name: 'teal',
+    lightColor: '#f0fdfa',
+    darkColor: '#134e4a',
+  },
+  {
+    name: 'cyan',
+    lightColor: '#ecfeff',
+    darkColor: '#164e63',
+  },
+  {
+    name: 'sky',
+    lightColor: '#f0f9ff',
+    darkColor: '#0c4a6e',
+  },
+  {
+    name: 'blue',
+    lightColor: '#eff6ff',
+    darkColor: '#1e3a8a',
+  },
+  {
+    name: 'indigo',
+    lightColor: '#eef2ff',
+    darkColor: '#312e81',
+  },
+  {
+    name: 'violet',
+    lightColor: '#f5f3ff',
+    darkColor: '#4c1d95',
+  },
+  {
+    name: 'purple',
+    lightColor: '#faf5ff',
+    darkColor: '#581c87',
+  },
+  {
+    name: 'fuchsia',
+    lightColor: '#fdf4ff',
+    darkColor: '#701a75',
+  },
+  {
+    name: 'pink',
+    lightColor: '#fdf2f8',
+    darkColor: '#831843',
+  },
+  {
+    name: 'rose',
+    lightColor: '#fff1f2',
+    darkColor: '#881337',
+  },
 ];
 
 export { colors };
