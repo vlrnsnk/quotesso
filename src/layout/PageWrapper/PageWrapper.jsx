d41d98ec5b599826a78a2f6431efb875c4c357e5
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-08-16 22:57:26 -0400

    Add transition to colors changing after getting new quote

--- src/layout/PageWrapper/PageWrapper.jsx
+++ src/layout/PageWrapper/PageWrapper.jsx
@@ -1,9 +1,13 @@
 const PageWrapper = ({ backgroundColor, children }) => {
   return (
-    <div className="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 flex
-      flex-col
+    <div
+      className="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14
+      flex flex-col
       justify-center
-      items-center" style={{ backgroundColor: backgroundColor, minHeight: '100vh' }}>
+      items-center
+      transition-colors motion-reduce:transition-none duration-1000"
+      style={{ backgroundColor: backgroundColor, minHeight: '100vh' }}
+    >
       {children}
     </div>
   );
